8b21a5a12d994c9515f9c2b13d690767efd19591
VIjay Karthik <mvijaykarthik@gmail.com>
2012-08-30 01:08:43 +0530

    Initial Commit

---
+++ VLSI_Assignment1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VLSI_Assignment1
+//
+//  Created by Vijay Karthik on 8/30/12.
+//  Copyright (c) 2012 Vijay Karthik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
